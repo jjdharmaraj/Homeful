75b6567c22c5aed29130a979f0d0bb47e1879d26
Chip Carnes <chip.carnes27@gmail.com>
2017-03-11 16:49:19 -0600

    remove index route

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 import configureStore from '~/state/configureStore';
 import HomePage from '~/components/HomePage';
 import CampList from '~/components/CampList';
